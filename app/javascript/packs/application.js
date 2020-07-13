b6fb5b2bf178a3db774cc91de447ee72ae55b86f
Mathieu Duprez <math.dups@gmail.com>
2020-07-13 04:24:42 +0200

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import '../stylesheets/application'
+import './bootstrap_custom.js'
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
